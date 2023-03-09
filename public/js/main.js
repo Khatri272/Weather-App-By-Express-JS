3749c20387645195071e9076204655ba237c259c
khatri_ayush <ayush.khatri@phpdots.com>
2023-03-09 18:13:31 +0530

    Change weather api

--- public/js/main.js
+++ public/js/main.js
@@ -15,22 +15,22 @@ const getInfo = async (event) => {
     } else {
         try {
             datahide.classList.remove("data_hide");
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&APPID=d12de75db6a2c0ce4cd7b4e0108fdee8`;
+            // let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&APPID=d12de75db6a2c0ce4cd7b4e0108fdee8`;
+            let url = `https://api.weatherapi.com/v1/current.json?key=2dffac0fae0a4fbdb34121146230903&q=${cityVal}&aqi=no`;
             const response = await fetch(url);
             const data = await response.json();
             const arrData = [data];
-            const temp_c = Math.round((arrData[0].main.temp) - 273.15);
 
-            city_name.innerHTML = `${arrData[0].name}, ${arrData[0].sys.country}`;
-            temp_real_val.innerText = temp_c;
-            const tempMood = arrData[0].weather[0].main;
+            city_name.innerHTML = `${arrData[0].location.name}, ${arrData[0].location.country}`;
+            temp_real_val.innerText = arrData[0].current.temp_c;
+            const tempMood = arrData[0].current.condition.text;
 
             //  Condition to check weather
-            if (tempMood == "Clear") {
+            if (tempMood == "Sunny") {
                 temp_status.innerHTML = "<i class='fa fa-sun fa-spin' style='color: #eccc68;' title='Clear'></i>";
-            } else if (tempMood == "Clouds") {
+            } else if (tempMood == "Partly cloudy" || tempMood == "Moderate or heavy rain with thunder" || tempMood == "Light rain") {
                 temp_status.innerHTML = "<i class='fa fa-cloud fa-beat-fade' style='color: #f1f2f6;' title='Clouds'></i>";
-            } else if (tempMood == "Rain") {
+            } else if (tempMood == "Light sleet" || tempMood == "Fog") {
                 temp_status.innerHTML = "<i class='fa fa-cloud-rain fa-beat-fade' style='color: #a4b0be;' title='Rainy'></i>";
             } else {
                 temp_status.innerHTML = "<i class='fa fa-sun fa-spin' style='color: #eccc68;'></i>";
